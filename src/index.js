72802edf0b6243a312afbd3ee02f9809efbe3491
Ted Trela <ted.trela@gmail.com>
2017-10-18 00:01:12 -0400

    Lotta changes
    User api with mock data
    Production build (partial)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import './index.css'
-import numeral from 'numeral';
+import './index.css';
+import { getUsers, deleteUser } from './api/userApi';
 
-/* eslint-disable no-console */
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`);
-
-const x = () => true;
-x();
+getUsers().then(result => {
+  let usersBody = '';
+  result.forEach(user => {
+    usersBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+        </tr>`;
+  });
+  global.document.getElementById('users').innerHTML = usersBody;
+  var deleteLinks = global.document.getElementsByClassName('deleteUser');
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes['data-id'].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
